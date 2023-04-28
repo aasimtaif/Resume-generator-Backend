bd2b3869c0b102ff5b1093ec9a1cdfc943faf804
Aasim Taif <taifullahansari@gmail.com>
2023-04-28 22:23:08 +0530

    solving the mongodb connect problem occured during deployment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true, }));
 
 const DB = process.env.MONGOBD_DATABASE
 
-mongoose.connect(DB).then(() => {
+mongoose.connect(DB,{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log("Connected to Database")
 }).catch(err => console.log(err, "error on connection"))
 
